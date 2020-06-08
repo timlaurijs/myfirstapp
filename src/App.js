1e368d2e97702f78fa250547ae3c697e6d3d709a
timlaurijs <timlaurijs@gmail.com>
2020-06-08 11:09:16 +0200

    added downhill

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Harco op de fiets roept iets van:
+          Harco rijd met de fiets downhill en roept iets van:
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Galema Galema
+          Wil koffie drinkn.
         </a>
       </header>
     </div>
